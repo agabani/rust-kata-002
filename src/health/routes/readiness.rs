6f003c63d4e0453deb65c23131270e062e10f1c8
agabani <agabani@users.noreply.github.com>
2020-12-30 17:27:03 +0000

    test: health unit test

---
+++ src/health/routes/readiness.rs
@@ -0,0 +1,18 @@
+use actix_web::HttpResponse;
+
+pub(crate) async fn get() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http;
+
+    #[actix_rt::test]
+    async fn it_returns_ok() {
+        let response = get().await;
+
+        assert_eq!(response.status(), http::StatusCode::OK);
+    }
+}
