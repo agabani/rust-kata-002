95a6b52f8537182b84cce926670356e98f468a47
agabani <agabani@users.noreply.github.com>
2020-12-30 17:41:49 +0000

    test: health integration test

--- src/health/routes/readiness.rs
+++ src/health/routes/readiness.rs
@@ -10,7 +10,7 @@ mod tests {
     use actix_web::http;
 
     #[actix_rt::test]
-    async fn it_returns_ok() {
+    async fn test_get_ok() {
         let response = get().await;
 
         assert_eq!(response.status(), http::StatusCode::OK);
