32822511637c7031224de54035016a583f18229d
agabani <agabani@users.noreply.github.com>
2021-01-01 02:19:23 +0000

    feat: distroless

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rust_kata_002::health::health_routes;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(web::scope("/health").configure(health_routes)))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
