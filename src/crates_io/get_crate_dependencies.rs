a597975e024fad357654ae018aacf898b978c3d2
agabani <agabani@users.noreply.github.com>
2021-01-14 18:26:39 +0000

    refactor: crates.io get crate dependency api

--- src/crates_io/get_crate_dependencies.rs
+++ src/crates_io/get_crate_dependencies.rs
@@ -8,36 +8,36 @@ impl CratesIoClient {
         &self,
         crate_name: &str,
         crate_version: &str,
-    ) -> RustKataResult<CrateDependenciesResponse> {
-        let string = format!(
+    ) -> RustKataResult<Response> {
+        let url = format!(
             "{}/api/v1/crates/{}/{}/dependencies",
             self.base_url, crate_name, crate_version
         );
 
-        let response = self.client.get(&string).send().await.unwrap();
+        let response = self.client.get(&url).send().await.unwrap();
 
         if response.status() != StatusCode::OK {
             return Err(RustKataError {});
         }
 
-        let json = response.json::<CrateDependenciesResponse>().await.unwrap();
+        let response = response.json().await.unwrap();
 
-        Ok(json)
+        Ok(response)
     }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct CrateDependenciesResponse {
+pub struct Response {
     #[serde(rename = "dependencies")]
-    dependencies: Vec<CrateDependencyResponse>,
+    dependencies: Vec<DependencyResponse>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct CrateDependencyResponse {
+pub struct DependencyResponse {
     #[serde(rename = "id")]
-    id: i32,
+    id: i64,
     #[serde(rename = "version_id")]
-    version_id: i32,
+    version_id: i64,
     #[serde(rename = "crate_id")]
     crate_id: String,
     #[serde(rename = "req")]
@@ -53,7 +53,7 @@ pub struct CrateDependencyResponse {
     #[serde(rename = "kind")]
     kind: String,
     #[serde(rename = "downloads")]
-    downloads: i32,
+    downloads: i64,
 }
 
 #[cfg(test)]
@@ -65,8 +65,54 @@ mod tests {
     async fn test() {
         let mock = mock("GET", "/api/v1/crates/rand/0.8.2/dependencies")
             .with_status(200)
-            .with_body(
-                r#"
+            .with_header("content-type", "application/json; charset=utf-8")
+            .with_body(RESPONSE)
+            .match_header(
+                "user-agent",
+                "rust-kata-002 (https://github.com/agabani/rust-kata-002)",
+            )
+            .create();
+
+        let client = CratesIoClient::new(&mockito::server_url()).unwrap();
+        let response = client
+            .get_crate_dependencies(&"rand", &"0.8.2")
+            .await
+            .unwrap();
+
+        assert_eq!(response.dependencies.len(), 10usize);
+
+        assert_eq!(response.dependencies[0].id, 2012432);
+        assert_eq!(response.dependencies[0].version_id, 326822);
+        assert_eq!(response.dependencies[0].crate_id, "bincode");
+        assert_eq!(response.dependencies[0].req, "^1.2.1");
+        assert_eq!(response.dependencies[0].optional, false);
+        assert_eq!(response.dependencies[0].default_features, true);
+        assert_eq!(
+            response.dependencies[0].features,
+            Some(Vec::<String>::new())
+        );
+        assert_eq!(response.dependencies[0].target, None);
+        assert_eq!(response.dependencies[0].kind, "dev");
+        assert_eq!(response.dependencies[0].downloads, 0);
+
+        assert_eq!(response.dependencies[9].id, 2012431);
+        assert_eq!(response.dependencies[9].version_id, 326822);
+        assert_eq!(response.dependencies[9].crate_id, "serde");
+        assert_eq!(response.dependencies[9].req, "^1.0.103");
+        assert_eq!(response.dependencies[9].optional, true);
+        assert_eq!(response.dependencies[9].default_features, true);
+        assert_eq!(
+            response.dependencies[9].features,
+            Some(vec!["derive".to_owned()])
+        );
+        assert_eq!(response.dependencies[9].target, None);
+        assert_eq!(response.dependencies[9].kind, "normal");
+        assert_eq!(response.dependencies[9].downloads, 0);
+
+        mock.assert();
+    }
+
+    const RESPONSE: &str = r#"
 {
     "dependencies": [
         {
@@ -195,51 +241,5 @@ mod tests {
         }
     ]
 }
-            "#,
-            )
-            .with_header("content-type", "application/json; charset=utf-8")
-            .match_header(
-                "user-agent",
-                "rust-kata-002 (https://github.com/agabani/rust-kata-002)",
-            )
-            .create();
-
-        let client = CratesIoClient::new(&mockito::server_url()).unwrap();
-        let response = client
-            .get_crate_dependencies(&"rand", &"0.8.2")
-            .await
-            .unwrap();
-
-        assert_eq!(response.dependencies.len(), 10usize);
-
-        assert_eq!(response.dependencies[0].id, 2012432);
-        assert_eq!(response.dependencies[0].version_id, 326822);
-        assert_eq!(response.dependencies[0].crate_id, "bincode");
-        assert_eq!(response.dependencies[0].req, "^1.2.1");
-        assert_eq!(response.dependencies[0].optional, false);
-        assert_eq!(response.dependencies[0].default_features, true);
-        assert_eq!(
-            response.dependencies[0].features,
-            Some(Vec::<String>::new())
-        );
-        assert_eq!(response.dependencies[0].target, None);
-        assert_eq!(response.dependencies[0].kind, "dev");
-        assert_eq!(response.dependencies[0].downloads, 0);
-
-        assert_eq!(response.dependencies[9].id, 2012431);
-        assert_eq!(response.dependencies[9].version_id, 326822);
-        assert_eq!(response.dependencies[9].crate_id, "serde");
-        assert_eq!(response.dependencies[9].req, "^1.0.103");
-        assert_eq!(response.dependencies[9].optional, true);
-        assert_eq!(response.dependencies[9].default_features, true);
-        assert_eq!(
-            response.dependencies[9].features,
-            Some(vec!["derive".to_owned()])
-        );
-        assert_eq!(response.dependencies[9].target, None);
-        assert_eq!(response.dependencies[9].kind, "normal");
-        assert_eq!(response.dependencies[9].downloads, 0);
-
-        mock.assert();
-    }
+    "#;
 }
